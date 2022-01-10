f35dbcac300a1b6d06c63a35f8f9e94776ea100a
Iryna Puchkova <irynapuchkova18@gmail.com>
2022-01-10 21:23:37 +0200

    refactor with hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,105 +1,107 @@
-import { Component } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
-import { fetchInfo } from '../../API/fetchimages';
-import Searchbar from '../Searchbar/Searchbar';
-import Button from '../Button/Button';
-import ImageGallery from '../ImageGallery/ImageGallery';
-import Modal from '../Modal/Modal';
-import Loader from '../Loader/Loader';
+import { fetchInfo } from "../../API/fetchimages";
+import Searchbar from "../Searchbar/Searchbar";
+import Button from "../Button/Button";
+import ImageGallery from "../ImageGallery/ImageGallery";
+import Modal from "../Modal/Modal";
+import Loader from "../Loader/Loader";
 
-import 'react-toastify/dist/ReactToastify.css';
-import { AppContainer } from './App.styled';
+import "react-toastify/dist/ReactToastify.css";
+import { AppContainer } from "./App.styled";
 
-export default class App extends Component {
-  state = {
-    images: [],
-    inputValue: '',
-    page: 1,
-    totalHits: null,
-    reqStatus: 'idle',
-    selectedImg: '',
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(null);
+  const [reqStatus, setReqStatus] = useState("idle");
+  const [selectedImg, setSelectedImg] = useState("");
+
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: "smooth",
+  });
+
+  const handleSearchbarSubmit = (value) => {
+    if (value.trim() === "") {
+      return toast("PlEASE ENTER YOUR QUERY");
+    }
+
+    if (value === inputValue) {
+      return;
+    }
+
+    setInputValue(value);
+    setPage(1);
+    setImages([]);
   };
 
-  async componentDidUpdate(_, prevState) {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-
-    const { inputValue, page } = this.state;
-
-    if (inputValue.trim() === '') {
-      return toast('PlEASE ENTER YOUR QUERY');
+  useEffect(() => {
+    if (inputValue.trim() === "") {
+      return;
+    } else {
+      (async function () {
+        try {
+          setReqStatus("pending");
+          const { hits, totalHits } = await fetchInfo(inputValue, 1);
+          toast(`We found ${totalHits} images`);
+          setImages(hits);
+          setTotalHits(totalHits);
+          setReqStatus("resolved");
+        } catch (error) {
+          setReqStatus("rejected");
+          console.error(error.message);
+        }
+      })();
     }
+  }, [inputValue]);
 
-    if (prevState.inputValue !== inputValue && inputValue.trim() !== '') {
-      try {
-        this.setState({ reqStatus: 'pending' });
-        const { hits, totalHits } = await fetchInfo(inputValue, 1);
-        toast(`We found ${totalHits} images`);
-
-        this.setState({
-          images: hits,
-          totalHits,
-          reqStatus: 'resolved',
-          page: 1,
-        });
-      } catch (error) {
-        this.setState({ reqStatus: 'rejected' });
-        console.error(error.message);
-      }
+  useEffect(() => {
+    if (page !== 1) {
+      (async function () {
+        try {
+          setReqStatus("pending");
+          const { hits } = await fetchInfo(inputValue, page);
+          setImages((prevHits) => [...prevHits, ...hits]);
+          setReqStatus("resolved");
+        } catch (error) {
+          setReqStatus("rejected");
+          console.error(error.message);
+        }
+      })();
     }
+  }, [inputValue, page]);
 
-    if (
-      prevState.inputValue === inputValue &&
-      prevState.page !== page &&
-      page !== 1
-    ) {
-      try {
-        this.setState({ reqStatus: 'pending' });
-        const { hits } = await fetchInfo(inputValue, page);
-        this.setState(prevState => ({
-          images: [...prevState.images, ...hits],
-          reqStatus: 'resolved',
-        }));
-      } catch (error) {
-        this.setState({ reqStatus: 'rejected' });
-        console.error(error.message);
-      }
-    }
-  }
-
-  onLoadmoreButtonClick = e => {
+  const onLoadmoreButtonClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.setState(prevState => ({ page: prevState.page + 1 }));
+      setPage((prevPage) => prevPage + 1);
     }
   };
 
-  toggleModal = () => {
-    this.setState(prevState => ({ selectedImg: !prevState.selectedImg }));
+  const toggleModal = () => {
+    setSelectedImg(!selectedImg);
   };
 
-  onSelectedImg = selectedImage => {
-    this.setState({ selectedImg: selectedImage });
+  const onSelectedImg = (selectedImage) => {
+    setSelectedImg(selectedImage);
   };
 
-  render() {
-    const { images, totalHits, selectedImg, reqStatus } = this.state;
-    const showBtnLoadMore = images.length >= 12 && images.length < totalHits;
-    return (
-      <AppContainer>
-        <Searchbar onSubmit={value => this.setState({ inputValue: value })} />
-        <ToastContainer role="alert" autoClose={2000} />
-        {reqStatus === 'pending' && <Loader />}
-        <ImageGallery data={images} onSelect={this.onSelectedImg} />
-        {showBtnLoadMore && <Button onClick={this.onLoadmoreButtonClick} />}
-        {selectedImg && (
-          <Modal onClose={this.toggleModal}>
-            <img src={selectedImg} alt="" />
-          </Modal>
-        )}
-      </AppContainer>
-    );
-  }
+  const showBtnLoadMore = images.length >= 12 && images.length < totalHits;
+
+  return (
+    <AppContainer>
+      <Searchbar onSubmit={handleSearchbarSubmit} />
+      <ToastContainer role="alert" autoClose={2000} />
+      {reqStatus === "pending" && <Loader />}
+      <ImageGallery data={images} onSelect={onSelectedImg} />
+      {showBtnLoadMore && <Button onClick={onLoadmoreButtonClick} />}
+      {selectedImg && (
+        <Modal onClose={toggleModal}>
+          <img src={selectedImg.toString()} alt="" />
+        </Modal>
+      )}
+    </AppContainer>
+  );
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,33 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import { Overlay, ModalWrapper } from "./Modal.styled";
 
 const modalRoot = document.querySelector("#modal-root");
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
+export default function Modal({ onClose, children }) {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Escape") {
+        onClose();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
 
-  handleKeyDown = (e) => {
-    if (e.code === "Escape") {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropClick = (e) => {
+  const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalWrapper>{this.props.children}</ModalWrapper>
-      </Overlay>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalWrapper>{children}</ModalWrapper>
+    </Overlay>,
+    modalRoot
+  );
 }
